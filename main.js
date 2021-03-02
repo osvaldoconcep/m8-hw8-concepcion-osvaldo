a5546228b91437c025376fab5eb38b36390162e6
ikarus-balsero <osvaldoconcep@gmail.com>
2021-03-02 08:44:55 -0500

    Showing sub menu when hovering on main menu or submenu content

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // It is always helpful to use comments in your code!
+
+/**
+ * Display the submenus for a given menu
+ * @param menu: id of sub menu to be displayed
+ */
+function displaySubMenu(menu) {
+    const element = document.getElementById(menu);
+    element.classList.add("show-menu");
+}
+
+/**
+ * Hide the submenus for a given menu
+ * @param menu: id of sub menu to be displayed
+ */
+function hideSubMenu(menu) {
+    const element = document.getElementById(menu);
+    element.classList.remove("show-menu");
+}
